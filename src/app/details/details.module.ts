421a67afff70cd787bee0b404a7602adb614f290
David Yepes <djvash01@gmail.com>
2023-07-24 21:03:56 -0500

    feat: 🎸 update list for events

--- src/app/details/details.module.ts
+++ src/app/details/details.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 
 import { DetailsRoutingModule } from './details-routing.module';
 import { DetailsComponent } from './details.component';
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
@@ -11,7 +12,8 @@ import { DetailsComponent } from './details.component';
   ],
   imports: [
     CommonModule,
-    DetailsRoutingModule
+    DetailsRoutingModule,
+    MatCardModule,
   ]
 })
 export class DetailsModule { }
